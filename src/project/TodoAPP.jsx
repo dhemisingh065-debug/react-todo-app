1fa43a4782c03ec26251c4302ef5430ea095b7bf
Amarjeet singh <amarjeetkumarsingh0009@gmail.com>
2026-06-19 02:42:19 +0530

    Build Todo App with add and delete functionality

--- src/project/TodoAPP.jsx
+++ src/project/TodoAPP.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
 const TodoApp = () => {
+  const [inpute, setinpute] = useState("")
+  const [todo, settodo] = useState([])
+  const delettodo = (c) => {const uppdate = todo.filter((todo,index) =>{return index !==c;
+
+  });
+      settodo(uppdate)}
+  const addtodos = () => {
+    if(inpute.trim() =="") return;
+    settodo([...todo,inpute]);
+    setinpute("");
+  }
   return (
     <div className="min-h-screen bg-blue-800 flex justify-center items-center">
       <div className="bg-green-600 w-150 p-6 rounded-xl shadow-lg">
@@ -12,31 +24,29 @@ const TodoApp = () => {
             type="text"
             placeholder="Enter a task..."
             className="flex-1 border border-gray-300 rounded-lg px-3 py-2 outline-none focus:border-blue-500"
+            value={inpute}
+            onChange={(e) =>setinpute(e.target.value)}
           />
 
-          <button className="bg-blue-600 text-white px-5 rounded-lg hover:bg-blue-700">
+          <button className="bg-blue-600 text-white px-5 rounded-lg hover:bg-blue-700 cursor-pointer"
+          type="submit" 
+          onClick={addtodos}>
             Add
           </button>
         </div>
 
         <ul className="space-y-3">
 
-          <li className="flex justify-between items-center border rounded-lg p-3">
-            <span>Learn React</span>
+         {todo.map((todo, index) => (
+           <li
+           key={index}
+            className="flex justify-between items-center border rounded-lg p-3" >
+            <span>{todo}</span>
 
-            <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">
+            <button className="cursor-pointer bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600" onClick={() => delettodo(index)}>
               Delete
             </button>
-          </li>
-
-          <li className="flex justify-between items-center border rounded-lg p-3">
-            <span>Learn JavaScript</span>
-
-            <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">
-              Delete
-            </button>
-          </li>
-
+          </li>))}
         </ul>
 
       </div>
