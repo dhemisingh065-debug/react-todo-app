ee410d0be92907b24d5f6692d8ab95dda42c42d3
Amarjeet singh <amarjeetkumarsingh0009@gmail.com>
2026-06-18 08:18:28 +0530

    Initial React and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold text-blue-600">Tailwind Working 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
